5ef57f71157180e9b83bcb31e6f815f38fdd002d
Vladyslav Rudniev <RudnievVV@gmail.com>
2019-12-20 17:46:49 +0200

    js search small fix

--- django_ecommerce/ecommerce/static/ecommerce/js/custom.js
+++ django_ecommerce/ecommerce/static/ecommerce/js/custom.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
         var input_value
         category_option = document.getElementById("search-category-select-option")[document.getElementById("search-category-select-option").selectedIndex].text
         input_value = document.getElementById("search-category-input").value
+        if (!input_value){
+            input_value = " "
+            };
         window.location.replace(
             window.location.origin + "/catalog-search/" + category_option + "-" + input_value + "/"
         );
@@ -17,6 +20,9 @@ $(document).ready(function(){
         var input_value
         category_option = document.getElementById("search-category-select-option-on-scroll")[document.getElementById("search-category-select-option-on-scroll").selectedIndex].text
         input_value = document.getElementById("search-category-input-on-scroll").value
+        if (!input_value){
+            input_value = " "
+            };
         window.location.replace(
             window.location.origin + "/catalog-search/" + category_option + "-" + input_value + "/"
         );
