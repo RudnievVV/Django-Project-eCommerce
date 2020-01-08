23be01f355312667f95e7c50ad7bd9ed631e54d4
Vladyslav Rudniev <RudnievVV@gmail.com>
2020-01-08 17:47:51 +0200

    checkout first step

--- django_ecommerce/cart/urls.py
+++ django_ecommerce/cart/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.cart_detail, name='cart-detail'),
     path('add/<int:product_id>/', views.cart_add, name='cart-add'),
     url(r'^remove/(?P<product_id>\d+)/$', views.cart_remove, name='cart-remove'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('checkout-<str:guest_order>/', views.checkout, name='checkout-guest'),
 ]
