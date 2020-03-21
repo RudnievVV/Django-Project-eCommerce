04f91f936aec986d59e6f8fd7c8db63389e841a0
RudnievVV <Kravk@08>
2020-03-22 01:26:01 +0200

    some adaptive + order model

--- django_ecommerce/django_ecommerce/settings.py
+++ django_ecommerce/django_ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'orders.apps.OrdersConfig',
     'widget_tweaks',
     'users.apps.UsersConfig',
     'cart.apps.CartConfig',
